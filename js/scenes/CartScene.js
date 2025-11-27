5a15c38c2b04cbb22f98117be2f3626a81b6b45d
Rajaion <herodany02@gmail.com>
2025-11-27 14:26:58 +0100

    added medicine interaction with mouse

--- js/scenes/CartScene.js
+++ js/scenes/CartScene.js
@@ -3,6 +3,15 @@ class CartScene extends Phaser.Scene{
         super({key: "CartScene"});
     }
 
+    points = 0;
+    pointsText = null;
+    adrenalina = null;
+    nacl = null;
+    adrenalinaText = null;
+    naclText = null;
+    pickedAdrenaline = false;
+    pickedNacl = false;
+
     preload(){
         this.load.image("Cart", "img/Cart.png");
         this.load.image("Elettrocardiogramma", "img/Elettrocardiogramma.png");
@@ -19,20 +28,64 @@ class CartScene extends Phaser.Scene{
         this.add.image(centerX * 0.90, centerY * 1.40, "PatientCart").setScale(0.8);
         const cart = this.add.image(centerX * 1.60, centerY * 1.70, "Cart").setScale(0.5).setInteractive({useHandCursor: "true"});
 
-        const adrenalina = this.add.image(cart.x - 30, cart.y - 50,  "Adrenalina").setScale(0.15).setInteractive({useHandCursor: true});
-        const nacl = this.add.image(cart.x + 30, cart.y - 50, "Nacl").setScale(0.15).setInteractive({useHandCursor: true});
+        this.adrenalina = this.add.image(cart.x - 30, cart.y - 50,  "Adrenalina").setScale(0.15).setOrigin(0.5, 0.5).setInteractive({useHandCursor: true});
+        this.nacl = this.add.image(-550, -550, "Nacl").setScale(0.15).setOrigin(0.5, 0.5).setInteractive({useHandCursor: true});
+
+        this.pointsText = this.add.text(centerX, centerY * 0.10, this.points, {
+            fontSize: "24px",
+            color: "#2c3e50",
+            fontFamily: "Arial"
+        })
+
+        this.adrenalinaText = this.add.text(-550, -550, "Adrenalina", {
+            fontSize: "18px",
+            color: "#2c3e50",
+            fontFamily: "Arial"
+        }).setOrigin(0.5, 0.5);
+
+        this.naclText = this.add.text(-550, -550, "Nacl", {
+            fontSize: "18px",
+            color: "#2c3e50",
+            fontFamily: "Arial"
+        }).setOrigin(0.5, 0.5);
 
         cart.on("pointerdown", () => {
-            adrenalina.x = cart.x + 30;
-            adrenalina.y = cart.y;
-            nacl.x = cart.x - 30;
-            nacl.y = cart.y;
+            this.adrenalina.x = cart.x + 30;
+            this.adrenalina.y = cart.y;
+            this.adrenalinaText.x = this.adrenalina.x;
+            this.adrenalinaText.y = this.adrenalina.y - 50;
+            this.nacl.x = cart.x - 30;
+            this.nacl.y = cart.y;
+            this.naclText.x = this.nacl.x;
+            this.naclText.y = this.nacl.y - 30;
+            cart.setInteractive({useHandCursor: false});
         });
+
+        this.adrenalina.on("pointerdown", () => {
+            this.pickedAdrenaline = !this.pickedAdrenaline;
+        });
+
+        this.nacl.on("pointerdown", () => {
+            this.pickedNacl = !this.pickedNacl;
+        })
         
     }
-
+        
     update(){
+        
+        if(this.pickedAdrenaline){
+            this.moveObjAndText(this.adrenalina, this.adrenalinaText);
+        }
+        if(this.pickedNacl){
+            this.moveObjAndText(this.nacl, this.naclText);
+        }
+    }
 
+    moveObjAndText(obj, text){
+        obj.x = this.input.x;
+        obj.y = this.input.y;
+        text.x = obj.x;
+        text.y = obj.y - 50;
     }
 
 }
