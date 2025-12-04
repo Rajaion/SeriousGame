0c3922c25a4676ddb5d5e58599dd10b8526a57f5
Rajaion <herodany02@gmail.com>
2025-12-04 12:43:33 +0100

    finished the EndScene

--- js/scenes/ReviewScene.js
+++ js/scenes/ReviewScene.js
@@ -13,58 +13,87 @@ class ReviewScene extends Phaser.Scene{
         const centerX = this.scale.width / 2;
         const centerY = this.scale.height / 2; 
 
+        this.createBox(centerX, centerY / 2, 450, 60);
+
         this.add.text(centerX, centerY / 2, "Vediamo gli errori fatti:", {
             fontSize: "24px",
             color: "#000000ff",
             align: "center",
-            padding: {x:30, y:15}
+            padding: {x:30, y:20}
         }).setOrigin(0.5);
 
+        this.createBox(centerX, centerY / 2 + 100, 600, 100);
+
+            this.add.text(centerX, centerY / 2 + 92, "Ospedale:\n\n", {
+                fontSize: "24px",
+                color: "#e22222ff",
+                align: "center",
+                padding: {x:30, y:30}
+           }).setOrigin(0.5);
+
         if(gameState.errors.Hospital === 0){
-           this.add.text(centerX, centerY / 2 + 100, "Non hai compiuto errori in ospedale \n nella sala del paziente, bravo!", {
+           this.add.text(centerX, centerY / 2 + 100, "\nNon hai compiuto errori in ospedale \n nella sala del paziente, bravo!", {
             fontSize: "24px",
             color: "#000000ff",
             align: "center",
-            padding: {x:30, y:15}
+            padding: {x:30, y:20}
            }).setOrigin(0.5);
         }else{
-            this.add.text(centerX, centerY / 2 + 100, "Attento, devi prima scegliere la opzione corretta da telefono \n per poi cliccare sul paziente ", {
+            this.add.text(centerX, centerY / 2 + 100, "\nAttento, dovevi prima scegliere la opzione corretta da telefono \n per poi cliccare sul paziente ", {
             fontSize: "24px",
             color: "#000000ff",
             align: "center",
-            padding: {x:30, y:15}
+            padding: {x:30, y:20}
            }).setOrigin(0.5);
         }
 
+        this.createBox(centerX, centerY / 2 + 200, 700, 80);
+
+            this.add.text(centerX, centerY / 2 + 198, "Paziente:\n\n", {
+                fontSize: "24px",
+                color: "#e22222ff",
+                align: "center",
+                padding: {x:30, y:20}
+           }).setOrigin(0.5);
+
         if(gameState.errors.Patient === 0){
-          this.add.text(centerX, centerY / 2 + 200, "Hai selezionato la sequenza corretta, good job! ", {
+          this.add.text(centerX, centerY / 2 + 200, "\nHai selezionato la sequenza corretta, good job! ", {
             fontSize: "24px",
             color: "#000000ff",
             align: "center",
-            padding: {x:30, y:15}
+            padding: {x:30, y:20}
            }).setOrigin(0.5);  
         }else{
-            this.add.text(centerX, centerY / 2 + 200, "La sequenza corretta è x, altrimenti (spiegare qui motivo ecc...)", {
+            this.add.text(centerX, centerY / 2 + 200, "\nLa sequenza corretta è x, altrimenti (spiegare qui motivo ecc...)", {
             fontSize: "24px",
             color: "#000000ff",
             align: "center",
-            padding: {x:30, y:15}
+            padding: {x:30, y:20}
            }).setOrigin(0.5);    
         }
 
+        this.createBox(centerX, centerY / 2 + 300, 750, 100);
+
+            this.add.text(centerX, centerY / 2 + 290, "Carro:\n\n", {
+                fontSize: "24px",
+                color: "#e22222ff",
+                align: "center",
+                padding: {x:30, y:20}
+           }).setOrigin(0.5);
+
         if(gameState.errors.Cart === 0){
-          this.add.text(centerX, centerY / 2 + 300, "Corretto devi dare al paziente in ordine: \n adrenalina e poi Nacl per X motivi ", {
+          this.add.text(centerX, centerY / 2 + 300, "\nCorretto devi dare al paziente in ordine: \n adrenalina e poi Nacl per X motivi ", {
             fontSize: "24px",
             color: "#000000ff",
             align: "center",
-            padding: {x:30, y:15}
+            padding: {x:30, y:20}
            }).setOrigin(0.5);  
         }else{
-            this.add.text(centerX, centerY / 2 + 300, "Sbagliato, l'ordine corretto era Adrenalina -> Nacl", {
+            this.add.text(centerX, centerY / 2 + 300, "\nSbagliato, l'ordine corretto era Adrenalina -> Nacl", {
             fontSize: "24px",
             color: "#000000ff",
             align: "center",
-            padding: {x:30, y:15}
+            padding: {x:30, y:20}
            }).setOrigin(0.5);    
         }
 
@@ -73,6 +102,15 @@ class ReviewScene extends Phaser.Scene{
 
     Update(){
 
+    }
+
+    createBox(valueX, valueY, width, height){
+        const box = this.add.graphics(valueX, valueY, width, height);
+        box.fillStyle(0xecf0f1, 1);
+        box.fillRoundedRect(valueX - (width / 2), valueY - (height / 2), width, height, 5); //disegna rettangolo nella posizione 600x e 300y con angolo arrotondati di 5 (maggiore valore -> maggiore arrotondamento)
+        box.lineStyle(2, 0x2c3e50, 1);
+        box.strokeRoundedRect(valueX - (width / 2), valueY - (height / 2), width , height, 5);
+        return box;
     }
 
 }
