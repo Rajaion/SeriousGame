19877a19c626ec21f4fdaf075732bfef971108ac
Rajaion <herodany02@gmail.com>
2025-12-17 10:31:11 +0100

    Test from mobile

--- js/script.js
+++ js/script.js
@@ -9,12 +9,18 @@ const config = {
     type: Phaser.AUTO,
     parent: 'game-container',
     scale: {
-        mode: Phaser.Scale.RESIZE, // IMPORTANTE: usa RESIZE
+        mode: Phaser.Scale.RESIZE,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: window.innerWidth,
-        height: window.innerHeight
+        height: window.innerHeight,
+        resolution: window.devicePixelRatio  // CHIAVE per la nitidezza
     },
     backgroundColor: '#4D5B8C',
+    render: {
+        pixelArt: false,
+        antialias: true,
+        roundPixels: false
+    },
     scene: [MenuScene, IntroScene, CartScene, EndScene, HospitalScene, PatientScene, ReviewScene] // Le tue scene
 };
 
